fb2af7a1a22a18a65a6b6fa6108eefc00bfd0cb2
Matias Giachello <matiasgiachello04@gmail.com>
2023-04-26 21:28:00 -0300

    Finalizando la tercer entrega, Local storage

--- main.js
+++ main.js
@@ -4,30 +4,52 @@ let arrayCarrito = [];
 const carritoContainer = document.getElementById("carrito-de-compras");
 const botonVaciar = document.getElementById('eliminar-carrito');
 const precioTotal = document.getElementById('precio-total');
-// Items para cumplir de la entrega
-// 1 - Funciones del carritto
-// 2 - Capturar eventos y salida por DOM de se agrego producto por ej.
-// 3 - Implementar un loading.
-// 4 - STORAGE y JSON.
-// 5 - Implementar libreria externa.
-
-// Abre entrega, 25 abril, hasta el 2 de mayo.
+let modalCounter = document.getElementById('btn-menu');
+let buttonCompra = document.getElementById("comprar-btn")
 
 
+buttonCompra.addEventListener('click', () => {
+    Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Gracias por tu compra!',
+        showConfirmButton: false,
+        timer: 1500
+    })
+    arrayCarrito.length = 0;
+    precioTotal.innerText = 0;
+    renderizarCarrito()
+})
+
+
+
+
+// DOMContentLoaded
 document.addEventListener("DOMContentLoaded", () => {
     boxCreate();
+    aJson();
+    obtenerObjetos();
 });
+
+
+function aJson() {
+    if (localStorage.getItem('cart')) {
+        arrayCarrito = JSON.parse(localStorage.getItem('cart'))
+        renderizarCarrito()
+    }
+}
+
 
 //.JSON
 async function obtenerObjetos() {
     const direccion = await fetch("./data.json");
     const response = await direccion.json();
-    // guardamos en el array la respuesta de los datos
+    // guarda en el array la respuesta de los datos
     productos = response;
     boxCreate();
 }
 
-obtenerObjetos();
+
 
 function boxCreate() {
     const contenedorProductos = document.getElementById("container-productos");
@@ -45,7 +67,13 @@ function boxCreate() {
         // agregamos funcionalidad al boton
         const agregar = document.getElementById(`button${prod.id}`);
         agregar.addEventListener("click", () => {
-            alert(`se agrego producto al carrito ${prod.nombre}`);
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: 'Tu producto fue agregado correctamente',
+                showConfirmButton: false,
+                timer: 1500
+            })
             pushearCarrito(`${prod.id}`);
         });
     });
@@ -68,6 +96,7 @@ function pushearCarrito(id) {
     } else {
         arrayCarrito.push({ ...producto, cantidad: 1 });
     }
+    localStorage.setItem('cart', JSON.stringify(arrayCarrito))
     renderizarCarrito();
 }
 // renderizamos el carrito
@@ -91,12 +120,19 @@ function renderizarCarrito() {
         carritoContainer.appendChild(productoContainer);
         const eliminar = document.getElementById(`eliminar${producto.id}`);
         eliminar.addEventListener("click", (id) => {
+            Swal.fire({
+                position: 'center',
+                icon: 'error',
+                title: 'Tu producto fue eliminado correctamente',
+                showConfirmButton: false,
+                timer: 1500
+            })
             eliminarDelCarrito(producto.id);
         });
 
 
         precioTotal.innerText = arrayCarrito.reduce((acc, producto) => acc + producto.cantidad * producto.precio, 0);
-        console.log(precioTotal.innerText);
+
     });
 }
 
@@ -117,6 +153,8 @@ function eliminarDelCarrito(id) {
     }
 
     precioTotal.textContent = arrayCarrito.reduce((acc, producto) => acc - producto.precio, 0);
+    localStorage.setItem('cart', JSON.stringify(arrayCarrito));
+    
 
     renderizarCarrito();
 }
@@ -127,6 +165,7 @@ botonVaciar.addEventListener('click', () => {
     alert('se vacio del carrito')
     arrayCarrito.length = 0;
     precioTotal.innerText = arrayCarrito.reduce((acc, producto) => acc - producto.precio, 0);
+    localStorage.setItem('cart', JSON.stringify(arrayCarrito))
     renderizarCarrito();
 })
 
